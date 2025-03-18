0b973531aa6311661b75d1b97bd03a2d5ed0712d
inosiyukiwtest <inosiyukiwtest@gmail.com>
2025-03-18 20:25:02 +0700

    feat: add home page data

--- src/app/components/NewProductsSection.tsx
+++ src/app/components/NewProductsSection.tsx
@@ -1,58 +1,98 @@
-"use client";
-import Link from "next/link";
-import ProductCard from "./ProductCard";
+"use client";;
+import { useEffect, useState } from "react";
+import { useGetCollections } from "@/hooks/api/useManageCollection";
+import { useRouter } from "next/navigation";
+import { formatPriceVND } from "@/lib/utils";
 
 export default function NewProductsSection() {
+  interface CollectionItem {
+    collectionId: string;
+    collectionName: string;
+    description: string;
+    startTime: string;
+    totalItem: number;
+    isActived: boolean;
+    rewards: string;
+    imagePath: string;
+    endTime: string;
+    blindBoxPrice: number;
+    discountBlindBoxPrice: number;
+  }
+
+  const [items, setItems] = useState<CollectionItem[]>([]);
+  const getData = useGetCollections();
+  const route = useRouter();
+  const fetchData = async () => {
+    const data = await getData.mutateAsync({
+      pageNumber: 1,
+      pageSize: 10,
+    });
+    setItems(data.result.items);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="my-10 md:my-16">
-      <p className="text-2xl md:text-4xl font-semibold text-center mb-2 md:mb-4">
-        Sản phẩm mới nhất
-      </p>
-      <p className="text-center text-gray-500 mb-6">
-        Khám phá những sản phẩm mới được ra mắt cùng cộng đồng
-      </p>
+     
+      <>
+        <h1 className="text-center text-red-600 text-3xl my-2 font-bold uppercase font-sans mb-10">
+          Túi mù tại MYBOX
+        </h1>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8 mb-10 flex-1">
+          {items.map((item) => (
+            <div
+              key={item.collectionId}
+              className="bg-white rounded-lg shadow-md flex flex-col items-center overflow-hidden cursor-pointer"
+              onClick={() => route.push(`/products/${item.collectionId}`)}
+            >
+              <img
+                src={item.imagePath || "/placeholder.svg"}
+                alt={item.collectionName}
+                className="h-[150px] object-cover w-full"
+              />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8 mb-10">
-        {NEW_PRODUCTS.slice(0, 8).map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
-      </div>
-      <Link href="/products" className="mx-auto px-10 w-fit underline block">
-        Xem thêm
-      </Link>
+              <div className="p-4 w-full flex-1 flex flex-col">
+                <h2 className="font-semibold mb-1">{item.collectionName}</h2>
+
+                <div className="flex justify-between items-center mb-2">
+                  <span className="text-sm font-medium">Còn lại:</span>
+                  <span className="text-sm font-bold text-red-500">
+                    {item.totalItem}
+                  </span>
+                </div>
+
+                <div className="flex justify-between items-center mb-2">
+                  <span className="text-sm font-medium">Giá túi mù:</span>
+                  <span className="text-sm">
+                    {formatPriceVND(item.blindBoxPrice ?? 0)}
+                  </span>
+                </div>
+                {item.discountBlindBoxPrice > 0 && (
+                  <div className="flex justify-between items-center mb-2">
+                    <span className="text-sm font-medium">Giá giảm:</span>
+                    <span className="text-sm text-green-600">
+                      {formatPriceVND(item.discountBlindBoxPrice ?? 0)}
+                    </span>
+                  </div>
+                )}
+                <div className="mt-auto">
+                  <span
+                    className={`text-xs font-semibold px-2 py-1 rounded ${
+                      item.isActived
+                        ? "bg-green-100 text-green-800"
+                        : "bg-red-100 text-red-800"
+                    }`}
+                  >
+                    {item.isActived ? "Đang mở bán" : "Đóng"}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
     </div>
   );
 }
 
-const NEW_PRODUCTS = [
-  {
-    id: "1",
-    image: "/mock-images/image1.png",
-    title: "Itar",
-    price: 100,
-  },
-  {
-    id: "2",
-    image: "/mock-images/image2.png",
-    title: "White Cap",
-    price: 100,
-  },
-  {
-    id: "3",
-    image: "/mock-images/image3.png",
-    title: "Jae Namaz",
-    price: 100,
-  },
-  {
-    id: "4",
-    image: "/mock-images/image4.png",
-    title: "Dates",
-    price: 100,
-  },
-  {
-    id: "5",
-    image: "/mock-images/image5.png",
-    title: "Miswak",
-    price: 100,
-  },
-];
